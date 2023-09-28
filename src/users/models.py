6499d8d17a00250f0edca451af9384304d84a688
Damir Kambarov <damir.kambarov@qualixdev.com>
2023-09-28 20:11:03 +0500

    deleted field added to user model

--- src/users/models.py
+++ src/users/models.py
@@ -11,8 +11,9 @@ class User(AbstractUser):
     """Пользователь."""
 
     register_date = models.DateField(verbose_name='Дата регистрации', auto_now_add=True)
-    gender = models.IntegerField(choices=GenderType.choices, verbose_name='Пол')
+    gender = models.IntegerField(choices=GenderType.choices, verbose_name='Пол', null=True)
     dob = models.DateField(blank=True, null=True, verbose_name='Дата рождения')
+    deleted = models.BooleanField(default=False)
 
     class Meta(AbstractUser.Meta):
         ordering = ('username',)
